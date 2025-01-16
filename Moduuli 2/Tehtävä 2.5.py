e7cf440758a9607b949688cdd34c782b79a98827
rennel <renne.lehto@metropolia.fi>
2025-01-16 15:23:03 +0200

    valmiit ja testatut

--- Moduuli 2/Tehtävä 2.5.py
+++ Moduuli 2/Tehtävä 2.5.py
@@ -1,22 +1,15 @@
-from idlelib.pyshell import fix_x11_paste
-
 leiviskä = input('Anna leivisköjen määrä: ')
 naula = input('Anna naulojen määrä: ')
 luoti = input('Anna luotien määrä: ')
-
 leiviskä = float(leiviskä)
 naula = float(naula)
 luoti = float(luoti)
-
 naula = (luoti * 32)
 leiviskä = (naula * 20)
-
 grammamuunnos = (leiviskä)+(naula)+(luoti)
-
 grammat = (grammamuunnos*13.3)
 Kilot = int(grammat/1000)
 loputgrammat = float(Kilot%1000)
-
 print (f'Paino nykyään olisi {Kilot} kiloa ja {loputgrammat} grammaa.')
 
 
