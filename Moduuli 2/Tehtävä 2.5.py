7419025c1eebc683ce8d3e77ad46f5beda2408a8
rennel <renne.lehto@metropolia.fi>
2025-01-16 11:55:26 +0200

    toka tehtävä

---
+++ Moduuli 2/Tehtävä 2.5.py
@@ -0,0 +1,21 @@
+Leiviskä = input('Anna leivisköjen määrä: ')
+naula = input('Anna naulojen määrä: ')
+luoti = input('Anna naulojen määrä: ')
+
+luoti = 13 * gramma
+naula = (luoti * 32)
+leiviskä = (naula * 20)
+kilo = 1000 * gramma
+
+väligrammat =
+
+print (f'Massa nykymittojen muukaan on {kilot} kiloa ja {grammat} grammaa.')
+
+
+>Kirjoita ohjelma, joka kysyy käyttäjältä massan keskiaikaisten mittojen mukaan
+>leivisköinä, nauloina ja luoteina. Ohjelma muuntaa syötteen täysiksi kilogrammoiksi
+>ja grammoiksi sekä ilmoittaa tuloksen käyttäjälle.
+
+ >   Yksi leiviskä on 20 naulaa.
+ >   Yksi naula on 32 luotia.
+  >  Yksi luoti on 13,3 grammaa.
