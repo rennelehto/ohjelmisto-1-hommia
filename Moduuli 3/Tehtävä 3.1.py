9d0c329f996796cbdc85875c27a9516fd1a897db
rennel <renne.lehto@metropolia.fi>
2025-01-21 13:23:41 +0200

    virheen korjauksia

---
+++ Moduuli 3/Tehtävä 3.1.py
@@ -0,0 +1,15 @@
+#Kirjoita ohjelma, joka kysyy kalastajalta kuhan
+# pituuden senttimetreinä. Jos kuha on alamittainen,
+# ohjelma käskee laskea kuhan takaisin järveen ilmoittaen
+# samalla käyttäjälle, montako senttiä alimmasta sallitusta
+# pyyntimitasta puuttuu. Kuha on alamittainen, jos sen pituus on alle 37 cm.
+
+import math
+pituus_str=input('Anna kuhan pituus senttimetreinä: ')
+pituus= float(pituus_str)
+määrä=37-pituus
+if pituus < 37:
+    print (f'Kuha on {määrä} cm alamittainen, laske se takaisin veteen.')
+else:
+    print ('Hyvä saalis!')
+
