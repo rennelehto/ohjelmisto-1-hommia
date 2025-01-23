06d5f0727bc289f0867890aa94d5c761b19b3af7
rennel <renne.lehto@metropolia.fi>
2025-01-23 16:20:52 +0200

    moduuli4

---
+++ Moduuli 5/Tehtävä 5.1.py
@@ -0,0 +1,7 @@
+'''Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
+Käytä for-toistorakennetta.'''
+
+import random
+
+nopat=[]
