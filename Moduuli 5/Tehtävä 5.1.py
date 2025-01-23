fc62e6abaa0c865086d87c4136b366d0852823f7
rennel <renne.lehto@metropolia.fi>
2025-01-23 16:56:25 +0200

    moduuli5

--- Moduuli 5/Tehtävä 5.1.py
+++ Moduuli 5/Tehtävä 5.1.py
@@ -3,5 +3,36 @@ Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
 Käytä for-toistorakennetta.'''
 
 import random
+import math
 
-nopat=[]
+'''noppaluku=[]
+nopat=(input('Kuinka montaa nopaa heitetään? '))
+noppa=(random.randint(1, 6))
+
+while nopat!='':
+    (noppaluku.append(nopat))
+    for 
+'''
+
+nopat=0
+tulokset=[]
+while nopat>0:
+    nopat = (input('Kuinka montaa nopaa heitetään? '))
+    tulokset.append(noppa)
+    noppa=((random.randint(1, 6))
+    nopat=nopat+1
+
+
+'''kerrat=0
+while kerrat<=5:
+    käyttis = input('Anna käyttäjätunnus: ')
+    salasana = input('Anna salasana: ')
+    if käyttis!='python' or salasana!='rules':
+        print('Pääsy evätty')
+        kerrat = kerrat + 1
+    else:
+        print('Tervetuloa')
+        break'''
+
+
+
