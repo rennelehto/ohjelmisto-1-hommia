f9f8330a481a985c2be6d2c162ebfe827e7fa75e
rennel <renne.lehto@metropolia.fi>
2025-01-23 18:22:24 +0200

    moduuli5

--- Moduuli 5/Tehtävä 5.1.py
+++ Moduuli 5/Tehtävä 5.1.py
@@ -17,15 +17,19 @@ while nopat!='':
     for 
 '''
 tulokset=[]
-heitot = 0
-while heitot>0:
-    heitot = (input('Kuinka montaa nopaa heitetään? '))
+
+heitot = int(input('Kuinka montaa nopaa heitetään? '))
+while heitot>=1:
+
     noppa = (random.randint(1, 6))
     tulokset.append(noppa)
-    if noppa>1:
-            noppa=noppa+noppa
+    if heitot>1:
+        heitot=heitot+1
+        print(f'{tulokset}')
+        break
 
-
+'''if noppa > 1:
+    noppa = noppa + noppa
 for vastaus in tulokset:
     vastaus=noppa
     print(f'Noppien silmälukujen summa on {vastaus}')
@@ -34,16 +38,4 @@ else:
     print('Vituiks meni.')
 
 
-'''kerrat=0
-while kerrat<=5:
-    käyttis = input('Anna käyttäjätunnus: ')
-    salasana = input('Anna salasana: ')
-    if käyttis!='python' or salasana!='rules':
-        print('Pääsy evätty')
-        kerrat = kerrat + 1
-    else:
-        print('Tervetuloa')
-        break'''
-
-
-
+'''
