9593afe81cc44495dbec799423a645309a071144
rennel <renne.lehto@metropolia.fi>
2025-01-23 13:40:56 +0200

    moduuli4

---
+++ Moduuli 4/Tehtävä 4.2.py
@@ -0,0 +1,12 @@
+'''Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä
+antaa negatiivisen tuumamäärän.
+Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm'''
+import math
+
+tuuma_str=input('Syötä tuumamäärä: ')
+tuuma=float(tuuma_str)
+
+while tuuma>0:
+    print(f'{tuuma} tuumaa on {tuuma*2.54} senttimetriä')
+    if tuuma<0:
+        break
