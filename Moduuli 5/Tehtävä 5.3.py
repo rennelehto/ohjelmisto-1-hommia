7e7ea8fb4f28cb5563119e5cc5725c2ad6f5f690
rennel <renne.lehto@metropolia.fi>
2025-01-25 11:07:39 +0200

    moduuli5

--- Moduuli 5/Tehtävä 5.3.py
+++ Moduuli 5/Tehtävä 5.3.py
@@ -1,6 +1,6 @@
 '''Kirjoita ohjelma, joka kysyy käyttäjältä kokonaisluvun ja ilmoittaa,
 onko se alkuluku. Tässä tehtävässä alkulukuja ovat luvut,
 jotka ovat jaollisia vain ykkösellä ja itsellään.'''
+
 import math
 luku=int(input('Syötä kokonaisluku: '))
-while luku
