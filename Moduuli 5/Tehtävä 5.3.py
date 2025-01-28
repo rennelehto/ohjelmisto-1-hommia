7e699fea611d24fe291dbb0ba4de74f1a1581f70
rennel <renne.lehto@metropolia.fi>
2025-01-28 15:14:36 +0200

    kolams tehtävä

--- Moduuli 5/Tehtävä 5.3.py
+++ Moduuli 5/Tehtävä 5.3.py
@@ -3,4 +3,11 @@ onko se alkuluku. Tässä tehtävässä alkulukuja ovat luvut,
 jotka ovat jaollisia vain ykkösellä ja itsellään.'''
 
 import math
-luku=int(input('Syötä kokonaisluku: '))
+
+luku_str = int(input('Anna kokonaisluku: '))
+for kerroin_str in range(2, luku_str, 1):
+    if luku_str%kerroin_str==0:
+        print('LUKU EI OLE ALKULUKU')
+        break
+else:
+    print('LUKU ON ALKULUKU')
