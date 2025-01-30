2f52f344d02ad181e68d9a5305cc2c60706967a2
rennel <renne.lehto@metropolia.fi>
2025-01-30 13:58:19 +0200

    kutonen jee

---
+++ Moduuli 6/Tehtävä 6.4.py
@@ -0,0 +1,13 @@
+#Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
+# Ohjelma palauttaa listassa olevien lukujen summan.
+# Kirjoita testausta varten pääohjelma, jossa luot listan,
+# kutsut funktiota ja tulostat sen palauttaman summan.
+
+def lista(luvut):
+    print(f'{summa}')
+    return summa
+
+numerot=[1,4,5,4]
+summa = numerot[0]+numerot[1]+numerot[2]+numerot[3]
+lista(numerot)
+
